14d7a30fd476af7ea4e0454dbc2ce39b40709812
Travis Brown <travjbrown98@gmail.com>
2021-06-14 22:07:40 -0400

    - dead code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,22 +2,10 @@ import React from 'react';
 import {  } from '@material-ui/icons';
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
   Link
 } from "react-router-dom";
 import './Home.css';
 import MenuIcon from '@material-ui/icons/Menu';
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
-import image1 from "./../portfolio/DSC_0223.jpg";
-import image2 from "./../portfolio/DSC_2504.jpg";
-import image3 from "./../portfolio/DSC_4172.jpg";
-import image4 from "./../portfolio/DSC_1322.jpg";
-import image5 from "./../portfolio/DSC_2263.jpg";
-import image6 from "./../portfolio/DSC_2511.jpg";
 import {SocialMediaIconsReact} from 'social-media-icons-react';
 
 
@@ -33,34 +21,16 @@ function Home() {
         </header>
         
         <main>
-          {/**Insert Image Slider 
-            - Make Awesome slider work
-            - else customize Alice Carousel
-          */}
-          
-          {/* <AliceCarousel autoPlay autoPlayInterval="3000">
-            
-            <img src={image2} className="sliderimg"/>
-            <img src={image3} className="sliderimg"/>
-            <img src={image4} className="sliderimg"/>
-        </AliceCarousel> */}
-
-          {/* <AwesomeSlider animation="cubeAnimation">
-            <div data-src={image2} />
-            <div data-src={image3} /> 
-            <div data-src={image1} />
-          </AwesomeSlider> */}
         </main>
 
         <footer style={{textAlign:"center", marginBottom:"10px"}}>
-          <div>{/**Insert contact me button */}
+          <div>
             <a className="contactBTN">
               <Link to="/contact">Contact Me</Link>
             </a>
-            
           </div>
-          <div className="socialMediaLinks">
 
+          <div className="socialMediaLinks">
             <SocialMediaIconsReact 
               iconSize="6" 
               size="40" 
@@ -87,7 +57,6 @@ function Home() {
               roundness="50%" 
               icon="linkedin" 
               url="https://linkedin.com/in/xprsn"/>
-
           </div>
         </footer>
       </body>
