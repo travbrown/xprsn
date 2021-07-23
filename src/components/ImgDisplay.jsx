a19a84ee38b607d2171aa60a1946210ec6ce289b
Travis Brown <travjbrown98@gmail.com>
2021-07-23 07:47:33 -0400

    displays an img from Firebase storage (if you change storage rules to 'request.auth == null')

---
+++ src/components/ImgDisplay.jsx
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react';
+import { Container} from '@material-ui/core';
+import { storage } from "../firebase";
+import firebase from 'firebase/app';
+import "firebase/storage";
+import sunset from "../portfolio/malik_profile.jpg";
+
+
+export const ImgDisplay = (images) => { 
+    const [url, setUrl] = useState('');
+
+    useEffect(()=>{
+        storageHandle();
+    },[]);
+
+    function storageHandle(){
+        const storage = firebase.storage();
+        const photographyRef = storage.ref('photography/ccc.jpg');
+        console.log('ref -->',photographyRef);
+        photographyRef.getDownloadURL()
+        .then((url) => {
+            // `url` is the download URL for 'images/stars.jpg'
+
+            // This can be downloaded directly:
+            console.log('We are here', url);
+            const xhr = new XMLHttpRequest();
+            xhr.responseType = 'blob';
+            xhr.onload = (event) => {
+            const blob = xhr.response;
+            };
+            xhr.open('GET', url);
+            xhr.send();
+            // Or inserted into an <img> element
+            const img = document.getElementById('myimg');
+            setUrl(url);
+            console.log('url:',url);
+            img.setAttribute('src', url);
+            
+
+        })
+        .catch((error) => {
+            // Handle any errors
+            console.log(error);
+        });
+
+    }
+
+ 
+    return (
+        <Container>
+            {/* pass props in thru 'images' */}
+            <img src={url}/>
+        </Container>
+    );
+};
