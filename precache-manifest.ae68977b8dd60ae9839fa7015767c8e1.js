8281f5bbb1c87e22b01bf90468bbffb71922d1dc
Travis Brown <travjbrown98@gmail.com>
2022-04-26 21:17:23 -0500

    Updates

---
+++ precache-manifest.ae68977b8dd60ae9839fa7015767c8e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aade602635741ef2df283471a64ee9f5",
+    "url": "/xprsn/index.html"
+  },
+  {
+    "revision": "7a3e068bd2a8c759988b",
+    "url": "/xprsn/static/css/2.4ed9e2b1.chunk.css"
+  },
+  {
+    "revision": "caaefaf194961d6900e0",
+    "url": "/xprsn/static/css/main.a951b631.chunk.css"
+  },
+  {
+    "revision": "7a3e068bd2a8c759988b",
+    "url": "/xprsn/static/js/2.d65f6f6d.chunk.js"
+  },
+  {
+    "revision": "554b3ee56d3b8e88410b002e5de57d8f",
+    "url": "/xprsn/static/js/2.d65f6f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caaefaf194961d6900e0",
+    "url": "/xprsn/static/js/main.35ff1177.chunk.js"
+  },
+  {
+    "revision": "db8c8628ea03103ed294",
+    "url": "/xprsn/static/js/runtime-main.fbfd1c78.js"
+  }
+]);
