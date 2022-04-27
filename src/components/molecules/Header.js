39f918184319e09a43839a157e433675dd9784e2
Travis Brown <travjbrown98@gmail.com>
2022-04-26 21:13:57 -0500

    gh page fixes

--- src/components/molecules/Header.js
+++ src/components/molecules/Header.js
@@ -8,9 +8,6 @@ export const Header = () => {
             <header className="App-header">
                 <div className="headerContainer">
                     <div>XPRSV</div>
-                    {/* <div>ART</div>
-                    <div>ABOUT</div>
-                    <div>THOUGHT</div> */}
                     <MenuIcon style={{ fontSize: 50 }}></MenuIcon>
                 </div>
             </header>
